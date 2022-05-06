a08fcbfdfcf8a041eaf9f8b08fc259125b3a2a8d
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-06 14:57:43 +0200

    vue-dischi

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <DischiContent/>
+  </div>
+  
+</template>
+
+<script>
+import DischiContent from './components/DischiContent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DischiContent
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
